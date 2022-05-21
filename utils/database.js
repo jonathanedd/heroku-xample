e71670b3367705a2ae27e75539297e6ce902a759
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-05-21 02:58:05 -0500

    Added dialegOptions to database

--- utils/database.js
+++ utils/database.js
@@ -11,13 +11,12 @@ const db = new Sequelize({
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
   logging: false,
-  // dialegOptions: process.env.NODE_ENV === 'productions'
-  //   ? {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+  dialectOptions: process.env.NODE_ENV === 'production' ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  } : {},
 });
 
 module.exports = { db };
